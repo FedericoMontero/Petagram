9fd58c4294253ca531184bfa17f7154de5b40ba2
Federico Montero <fede17.m@gmail.com>
2020-10-18 21:05:36 -0300

    entrega 3

--- app/src/main/java/com/fedemcode/petagram/ListadoMascotas.java
+++ app/src/main/java/com/fedemcode/petagram/ListadoMascotas.java
@@ -1,14 +1,58 @@
 package com.fedemcode.petagram;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ListadoMascotas extends AppCompatActivity {
+
+    private ArrayList<DataSet> mascotas;
+    private RecyclerView listadoMascotas;
+    Toolbar toolbar;
+    private TextView titleToolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.listado_mascotas);
+
+        listadoMascotas = (RecyclerView) findViewById(R.id.rvMascotas1);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+
+        listadoMascotas.setLayoutManager(llm);
+        inicializarListaMascotas();
+        inicializarAdaptador();
+    }
+
+    public MascotasAdaptador adaptador;
+    public void inicializarAdaptador(){
+        adaptador = new MascotasAdaptador(mascotas,this);
+        listadoMascotas.setAdapter(adaptador);
+    }
+
+    public void inicializarListaMascotas(){
+        mascotas = new ArrayList<DataSet>();
+
+        mascotas.add(new DataSet("Perro y hueso","3",R.drawable.perros_uno));
+        mascotas.add(new DataSet("San Bernardo","4",R.drawable.perros_dos));
+        mascotas.add(new DataSet("Rocky","4",R.drawable.perros_tres));
+        mascotas.add(new DataSet("Husky Siberiano","4",R.drawable.perros_cuatro));
+        mascotas.add(new DataSet("Cschorro","4",R.drawable.perros_cinco));
     }
 }
--- app/src/main/java/com/fedemcode/petagram/MainActivity.java
+++ app/src/main/java/com/fedemcode/petagram/MainActivity.java
@@ -1,14 +1,110 @@
 package com.fedemcode.petagram;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<DataSet> mascotas;
+    private RecyclerView listadoMascotas;
+    Toolbar toolbar;
+    private ImageButton estrella;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        listadoMascotas = (RecyclerView) findViewById(R.id.rvMascotas);
+        estrella = (ImageButton) findViewById(R.id.estrella);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+
+        listadoMascotas.setLayoutManager(llm);
+        inicializarListaMascotas();
+        inicializarAdaptador();
+
+        estrella.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ListadoMascotas.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Hola estas en la App Mascotas", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        // noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public MascotasAdaptador adaptador;
+    public void inicializarAdaptador(){
+        adaptador = new MascotasAdaptador(mascotas,this);
+        listadoMascotas.setAdapter(adaptador);
+    }
+
+    public void inicializarListaMascotas(){
+        mascotas = new ArrayList<DataSet>();
+
+        mascotas.add(new DataSet("Perro con hueso","3",R.drawable.perros_uno));
+        mascotas.add(new DataSet("San Bernardo","4",R.drawable.perros_dos));
+    }
+
 }
+
+
+//El git se hace desde la consola y lo guarda en Github, petagram. si push no funciona leer el error y hacer git push master o algo asi
